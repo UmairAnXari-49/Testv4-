b7b04475cfd63c8e615ba9999fdef0498fa379d0
UmairAnXari-49 <93900172+UmairAnXari-49@users.noreply.github.com>
2023-12-31 07:04:55 +0500

    Update ContactSection.jsx

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactSection.css";
 import backgroundVideo from "../../assets/backgroundVideo.mp4";
 
 function ContactSection() {
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const response = await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      if (response.ok) {
+        console.log("Form submitted successfully");
+      } else {
+        console.error("Form submission failed");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  const handleSendButtonClick = () => {
+    // Programmatically trigger form submission
+    const form = document.forms["contact"];
+    if (form) {
+      form.submit();
+    }
   };
 
   return (
@@ -28,18 +63,52 @@ function ContactSection() {
 
             <h1>Let's talk</h1>
 
-            <input type="email" name="email" placeholder="Email" required />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
 
-            <input type="text" name="subject" placeholder="Subject" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
 
             <textarea
               name="message"
               placeholder="Your message"
+              value={formData.message}
+              onChange={handleChange}
               required
             ></textarea>
 
             <button type="submit" className="send-button">
               Send
+            </button>
+
+            {/* Add a button to trigger form submission */}
+            <button
+              type="button"
+              className="send-button"
+              onClick={handleSendButtonClick}
+            >
+              Send Programmatically
             </button>
           </form>
         </div>
