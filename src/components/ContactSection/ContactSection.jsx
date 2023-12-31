6b46a2e1037c66f6e2d59f5d36358137c3b53362
UmairAnXari-49 <93900172+UmairAnXari-49@users.noreply.github.com>
2023-12-31 07:13:10 +0500

    Update ContactSection.jsx

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,120 +1,18 @@
-import React, { useState } from "react";
-import "./ContactSection.css";
-import backgroundVideo from "../../assets/backgroundVideo.mp4";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tereza Petrovičová</title>
+  </head>
+  <body>
 
-function ContactSection() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-      });
-
-      if (response.ok) {
-        console.log("Form submitted successfully");
-      } else {
-        console.error("Form submission failed");
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
-
-  const handleSendButtonClick = () => {
-    // Programmatically trigger form submission
-    const form = document.forms["contact"];
-    if (form) {
-      form.submit();
-    }
-  };
-
-  return (
-    <div className="contact-section" id="contact">
-      <div className="background-title-contact">CONTACT</div>
-
-      <div className="form-container">
-        <video autoPlay loop muted className="contact-form-video">
-          <source src={backgroundVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-
-        <div className="form-overlay">
-          <form
-            onSubmit={handleSubmit}
-            name="contact"
-            method="POST"
-            data-netlify="true"
-          >
-            <input type="hidden" name="form-name" value="contact" />
-
-            <h1>Let's talk</h1>
-
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-
-            <input
-              type="text"
-              name="subject"
-              placeholder="Subject"
-              value={formData.subject}
-              onChange={handleChange}
-              required
-            />
-
-            <textarea
-              name="message"
-              placeholder="Your message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-
-            <button type="submit" className="send-button">
-              Send
-            </button>
-
-            {/* Add a button to trigger form submission */}
-            <button
-              type="button"
-              className="send-button"
-              onClick={handleSendButtonClick}
-            >
-              Send Programmatically
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default ContactSection;
+<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
+    
+    <div id="root"></div>
+  </body>
+</html>
